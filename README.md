a56286ec3091a11f201a5c5416c8267439d5e9ec
Philip Ganchev <philip.ganchev@ontotext.com>
2025-04-06 23:17:04 +0300

    Separete endpoints analyze/url and analyze/file

--- README.md
+++ README.md
@@ -10,9 +10,14 @@ See [instructions](file:///challenge.md)
 # Running
 
 1. Run `poetry run uvicorn paper_analyzer.api:app --reload`
-1. POST a PDF file to `/page-count` endpoint
+1. POST a paper URL to endpoint `/analyze-url`
     ```
-    curl -X POST "http://localhost:8000/extract-text"\
-    -H "Content-Type: application/json"\
-    -d '{"url": "https://arxiv.org/pdf/2101.08809"}'
+    curl -X POST 'http://localhost:8000/analyze/url'\
+    -d '{"url": "https://arxiv.org/pdf/2101.08809"}'\
+    -H 'Content-Type: application/json'
+    ```
+1. POST a paper PDF file to endpoint `/analyze/file`
+    ```
+    curl -X POST 'http://localhost:8000/analyze-file'\
+    -F 'file=@data/97479/paper.pdf'
     ```
--- paper_analyzer/api.py
+++ paper_analyzer/api.py
@@ -2,34 +2,48 @@ from io import BytesIO
 
 import PyPDF2
 import requests
-from fastapi import FastAPI, HTTPException
+from fastapi import Body, FastAPI, File, HTTPException, UploadFile
 from pydantic import BaseModel
 
-from paper_analyzer.analyzer import analyze
+from paper_analyzer import analyzer
+
 
 app = FastAPI()
 
 
-class PaperRequest(BaseModel):
-    url: str
+def _analyze_pdf(content):
+    try:
+        page_texts = analyzer.analyze(content)
+        return {'text': page_texts}
+    except PyPDF2.errors.PdfReadError:
+        raise HTTPException(400, 'Invalid PDF content')
+    #except Exception as e:
+    #    raise HTTPException(500, str(e))
 
 
-@app.post('/extract-text')
-async def extract_text(request: PaperRequest):
-    url = request.url
+@app.post('/analyze/file')
+async def analyze_file(file: UploadFile = File(...)):
+    if file.content_type != 'application/pdf':
+        raise HTTPException(400, f'Expected PDF, got {file.content_type}')
+    try:
+        content = await file.read()
+    finally:
+        await file.close()
+    return _analyze_pdf(BytesIO(content))
+
+
+@app.post('/analyze/url')
+async def analyze_url(url: str = Body(embed=True)):
     try:
         response = requests.get(url)
         response.raise_for_status()
-        content = BytesIO(response.content)
-        page_texts = analyze(content)
-        return {'text': page_texts}
-    except requests.RequestException:
-        msg = 'Failed to download the PDF from the store'
-        raise HTTPException(status_code=400, detail=msg)
-    except PyPDF2.errors.PdfReadError:
-        raise HTTPException(status_code=400, detail='Invalid PDF content')
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    except requests.RequestException as e:
+        raise HTTPException(400, f'Download failed: {str(e)}')
+    content_type = response.headers['content-type']
+    if content_type != 'application/pdf':
+        raise HTTPException(400, f'Expected PDF, got {content_type}')
+    return _analyze_pdf(BytesIO(response.content))
+
 
 
 @app.get('/')
